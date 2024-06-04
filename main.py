e0726c494254fe9f42dfb16f1c2ef82c94388194
QueenZ215 <ZoeCoolbaugh@gmail.com>
2024-06-04 02:43:23 -0700

    Done

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 mission_names = ['Apollo 11', 'Challenger', 'Curiosity Rover', 'Viking 1', 'Mars Pathfinder', 'Hubble Telescope', 'Apollo 13']
 mission_years = [1969, 1986, 2012, 1975, 1996, 1990, 1970]
 mission_success = [True, False, True, True, True, True, False]
+totalMission=0
+sucess=0
+year=2000
+
+for i,v in enumerate(mission_names): 
+	totalMission += 1 
+	if mission_success[i] == True:
+		sucess +=1
+sucessRate = (sucess / totalMission) * 100
+
+print(f"Total number of missions: {totalMission}")
+print(f"Number of successful missions: {sucess:.2f}%")
+print(f"Success rate: {sucessRate}")
+print(f"Missions launched before the year {year}")
+
+for i,v in enumerate(mission_names):
+	if mission_years[i] < year:
+		print(f"-  {mission_names[i].ljust(20)}{year- mission_years[i]}")
